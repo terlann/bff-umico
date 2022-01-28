beca470975b3be1efdf7c7f26454e0b51ce64929
MikayilovOE <orxan.mikayilov@kapitalbank.az>
2022-01-28 19:50:25 +0400

    change some field types

--- src/main/java/az/kapitalbank/bffumico/mapper/OrderMapper.java
+++ src/main/java/az/kapitalbank/bffumico/mapper/OrderMapper.java
@@ -9,14 +9,12 @@ import az.kapitalbank.bffumico.dto.request.ReverseRequestDto;
 import az.kapitalbank.bffumico.dto.response.CheckOrderResponseDto;
 import az.kapitalbank.bffumico.dto.response.CreateOrderResponseDto;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface OrderMapper {
 
     CreateOrderRequest toCreateOrderRequest(CreateOrderRequestDto createOrderRequestDto);
 
-    @Mapping(target = "trackId", source = "marketplaceTrackId")
     CreateOrderResponseDto toCreateOrderResponseDto(CreateOrderResponse body);
 
     CheckOrderResponseDto toCheckOrderResponseDto(CheckOrderResponse checkOrderResponse);
