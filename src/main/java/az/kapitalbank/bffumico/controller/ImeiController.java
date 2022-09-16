b52c9c7196c11b722a925cc18c110487e16e3269
tarlan <tarlan.qurbanov@kapitalbank.az>
2022-09-16 11:48:50 +0400

    NPO-855 new api for imeiCodes

--- src/main/java/az/kapitalbank/bffumico/controller/ImeiController.java
+++ src/main/java/az/kapitalbank/bffumico/controller/ImeiController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ public class ImeiController {
 
     ImeiService imeiService;
 
+    @PostMapping
     public ResponseEntity<Void> saveImeiCode(
             @Valid @RequestBody ImeiCodeRequestDto imeiCodeRequestDto) {
         imeiService.saveImei(imeiCodeRequestDto);
