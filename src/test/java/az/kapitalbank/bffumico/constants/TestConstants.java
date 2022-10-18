c2ce9d3998591969822e60e1cabe4ac1a1d18ca0
MikayilovOE <orxan.mikayilov@kapitalbank.az>
2022-10-18 15:46:17 +0400

    NPO-948 controller test

--- src/test/java/az/kapitalbank/bffumico/constants/TestConstants.java
+++ src/test/java/az/kapitalbank/bffumico/constants/TestConstants.java
@@ -7,17 +7,10 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum TestConstants {
     PIN("1234567"),
-    MOBILE_NUMBER("994551112233"),
-    RRN("29703c3e-9e41-11ec-b909-0242ac120002"),
-    DVS_ID("eb813fa4-a142-11ec-b925-0242ac120002"),
-    TASK_ID("3a30a65a-9bec-11ec-b909-0242ac120002"),
-    CARD_UID("d2a9d8bc-9beb-11ec-b909-0242ac120002"),
     TRACK_ID("3a30a65a-9bec-11ec-b909-0242ac120002"),
     CUSTOMER_ID("30d8c6d0-a0fd-11ec-b909-0242ac120002"),
     UMICO_USER_ID("30d8c6d0-3242-11ec-b909-0242ac120002"),
-    BUSINESS_KEY("d279220e-a1e9-11ec-b909-0242ac120002"),
-    TELESALES_ORDER_ID("eb813fa4-a142-11ec-b909-0242ac120002"),
-    TRANSACTION_ID("eb813fa4-a142-11ec-b925-0242ac120002");
+    TELESALES_ORDER_ID("eb813fa4-a142-11ec-b909-0242ac120002");
 
     final String value;
 
