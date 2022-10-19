5738168e207bf52888728c3c50aff2bfebf4b37f
MikayilovOE <orxan.mikayilov@kapitalbank.az>
2022-10-19 09:50:49 +0400

    NPO-948 controller test

--- src/test/java/az/kapitalbank/bffumico/controller/CustomerControllerTest.java
+++ src/test/java/az/kapitalbank/bffumico/controller/CustomerControllerTest.java
@@ -4,11 +4,15 @@ import static az.kapitalbank.bffumico.constants.TestConstants.CUSTOMER_ID;
 import static az.kapitalbank.bffumico.constants.TestConstants.PIN;
 import static az.kapitalbank.bffumico.constants.TestConstants.UMICO_USER_ID;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import az.kapitalbank.bffumico.dto.response.BalanceResponseDto;
 import az.kapitalbank.bffumico.service.CustomerService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.math.BigDecimal;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,9 +36,15 @@ class CustomerControllerTest {
 
     @Test
     void getCustomerBalance() throws Exception {
+        var response = BalanceResponseDto.builder().availableBalance(BigDecimal.ONE).build();
+
+        when(customerService.getCustomerBalance(UMICO_USER_ID.getValue(),
+                UUID.fromString(CUSTOMER_ID.getValue()))).thenReturn(response);
+
         mockMvc.perform(get("/api/v1/customers/balance")
                         .param("umico_user_id", UMICO_USER_ID.getValue())
                         .param("customer_id", CUSTOMER_ID.getValue()))
+                .andExpect(jsonPath("$.available_balance").value(response.getAvailableBalance()))
                 .andExpect(status().isOk());
 
         verify(customerService).getCustomerBalance(UMICO_USER_ID.getValue(),
--- src/test/java/az/kapitalbank/bffumico/controller/OrderControllerTest.java
+++ src/test/java/az/kapitalbank/bffumico/controller/OrderControllerTest.java
@@ -1,16 +1,23 @@
 package az.kapitalbank.bffumico.controller;
 
 import static az.kapitalbank.bffumico.constants.TestConstants.TELESALES_ORDER_ID;
+import static az.kapitalbank.bffumico.constants.TestConstants.TRACK_ID;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import az.kapitalbank.bffumico.dto.request.CreateOrderRequestDto;
 import az.kapitalbank.bffumico.dto.request.PurchaseRequestDto;
 import az.kapitalbank.bffumico.dto.request.RefundRequestDto;
 import az.kapitalbank.bffumico.dto.request.TelesalesResultRequestDto;
+import az.kapitalbank.bffumico.dto.response.CheckOrderResponseDto;
+import az.kapitalbank.bffumico.dto.response.CreateOrderResponseDto;
 import az.kapitalbank.bffumico.service.OrderService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -35,10 +42,16 @@ class OrderControllerTest {
     @Test
     void createOrder() throws Exception {
         var request = CreateOrderRequestDto.builder().build();
+        var response = CreateOrderResponseDto.builder()
+                .trackId(UUID.fromString(TRACK_ID.getValue()))
+                .build();
+
+        when(orderService.create(request)).thenReturn(response);
 
         mockMvc.perform(post("/api/v1/orders")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
+                .andExpect(jsonPath("$.track_id", is(response.getTrackId().toString())))
                 .andExpect(status().isCreated());
 
         verify(orderService).create(request);
@@ -58,7 +71,13 @@ class OrderControllerTest {
 
     @Test
     void checkOrder() throws Exception {
+        var response = CheckOrderResponseDto.builder()
+                .telesalesOrderId(TELESALES_ORDER_ID.getValue()).build();
+
+        when(orderService.check(TELESALES_ORDER_ID.getValue())).thenReturn(response);
+
         mockMvc.perform(post("/api/v1/orders/check/" + TELESALES_ORDER_ID.getValue()))
+                .andExpect(jsonPath("$.telesales_order", is(response.getTrackId().toString())))
                 .andExpect(status().isOk());
 
         verify(orderService).check(TELESALES_ORDER_ID.getValue());
